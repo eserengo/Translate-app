e39c43b6db62aeac951db0067b15f0eb6168c482
eserengo <eserengo@gmail.com>
2023-11-15 17:43:07 -0300

    Added base files: vite, vue, sass, assets

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "App",
+}
+</script>
+
+<template>
+  <h1>Hello</h1>
+</template>
+
+<style lang="scss">
+@import "./main.scss";
+
+h1 {
+  color: $clr-blue;
+}
+</style>
